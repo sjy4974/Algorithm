a8870015a02a3b5fe00b457bb09e0520653d2cc9
Sun <75000830+sjy4974@users.noreply.github.com>
2022-09-20 14:30:18 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 300 ms, Memory: 12444 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[][] map;
+	static int N;
+	static int[] dr = { 0, 1, 1 };
+	static int[] dc = { 1, 1, 0 };
+	static boolean[][] visited;
+	static int position = 0;
+	static int cnt;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		visited[0][0] = true;
+		visited[0][1] = true;
+		StringTokenizer st;
+
+		for (int r = 0; r < N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for (int c = 0; c < N; c++) {
+				map[r][c] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		powerset(0,1);
+		System.out.println(cnt);
+	}
+
+	static void powerset(int r, int c) {
+		if(r == N-1 && c == N-1) {
+			cnt++;
+			return;
+		}
+		else {
+			for(int d = 0; d < 3; d++) {
+				if(position == 0 && d == 2) continue;
+				if(position == 1 && d == 0) continue;
+				int nr = r+dr[d];
+				int nc = c+dc[d];
+				
+				if(nr < N && nc <N && !visited[nr][nc]) {
+					if( d == 0) {
+						if(map[nr][nc] == 1) continue;
+						int temp = position;
+						position = 0;
+						visited[nr][nc] = true;
+						powerset(nr,nc);
+						visited[nr][nc] = false;
+						position = temp;
+					} else if (d == 1) {
+						if(map[nr][nc] == 1 || (nr-1 >= 0 && map[nr-1][nc] == 1) || (nc-1 >= 0 && map[nr][nc-1] == 1)) continue;
+						int temp = position;
+						position = 2;
+						visited[nr][nc] = true;
+						powerset(nr,nc);
+						visited[nr][nc] = false;
+						position = temp;
+					} else if (d == 2) {
+						if(map[nr][nc] == 1) continue;
+						int temp = position;
+						position = 1;
+						visited[nr][nc] = true;
+						powerset(nr,nc);
+						visited[nr][nc] = false;
+						position = temp;
+					}
+				}
+			}
+		}
+
+	}
+
+}

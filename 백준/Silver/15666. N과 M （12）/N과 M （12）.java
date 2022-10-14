0a8a394aaa9b1b8f9b338244d63b87877b3d7f8f
Sun <75000830+sjy4974@users.noreply.github.com>
2022-10-14 12:59:31 +0900

    [Silver II] Title: N과 M (12), Time: 244 ms, Memory: 17912 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] arr;
+	static int[] result;
+	static int N, M;
+	static LinkedHashSet<String> hs = new LinkedHashSet<>();
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		result = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+		comb(0,0,"");
+
+		for(String s : hs) {
+			System.out.println(s);
+		}
+	}
+	public static void comb(int idx,int dept,String str) {
+		// 기저
+		if (dept == M) {
+			hs.add(str);
+			return;
+		}
+		for(int i = idx; i <N; i++) {
+			result[dept] = arr[i];
+			comb(i,dept+1,str+result[dept]+" ");
+		}
+	}
+
+}

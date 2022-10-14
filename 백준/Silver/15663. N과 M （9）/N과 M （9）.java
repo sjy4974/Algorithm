cd7eafb3b9572772289065751cabcb1f0c3c1dfd
Sun <75000830+sjy4974@users.noreply.github.com>
2022-10-14 11:56:22 +0900

    [Silver II] Title: N과 M (9), Time: 596 ms, Memory: 36752 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] arr;
+	static int[] result;
+	static boolean[] visited;
+	static int N, M;
+	static LinkedHashSet<String> hs = new LinkedHashSet<>();
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		result = new int[M];
+		visited = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+		perm(0,"");
+
+		for(String s : hs) {
+			System.out.println(s);
+		}
+		
+	}
+
+	public static void perm(int dept,String str) {
+		// 기저
+		if (dept == M) {
+			hs.add(str);
+			return;
+		}
+		for(int i = 0; i <N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				result[dept] = arr[i];
+				perm(dept+1,str+result[dept]+" ");
+				visited[i] = false;
+			}
+		}
+
+	}
+
+}

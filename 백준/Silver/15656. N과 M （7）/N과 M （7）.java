4f927b5549945e29d16b68a1f7064373aad31635
Sun <75000830+sjy4974@users.noreply.github.com>
2022-10-14 10:37:17 +0900

    [Silver III] Title: N과 M (7), Time: 860 ms, Memory: 407740 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] arr;
+	static int[] result;
+	static int N, M;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N];
+		result = new int[M];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		perm(0);
+		System.out.println(sb.toString());
+	}
+
+	public static void perm(int dept) {
+		// 기저
+		if (dept == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(result[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i = 0; i <N; i++) {
+			result[dept] = arr[i];
+			perm(dept+1);
+	
+		}
+
+	}
+
+}
